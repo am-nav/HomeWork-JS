573fcae8445671f31f7fb98829bdcb193e07598a
am-nav <141391730+am-nav@users.noreply.github.com>
2024-11-10 02:24:50 +0400

    Add files via upload

---
+++ Sorting/src/scripts/sorting.js
@@ -0,0 +1,129 @@
+const input = document.getElementById("input-numbers");
+const bubbleSortBtn = document.getElementById("bubble-sort");
+const selectionSortBtn = document.getElementById("selection-sort");
+const insertionSortBtn = document.getElementById("insertion-sort");
+const mergeSortBtn = document.getElementById("merge-sort");
+const result = document.getElementById('result');
+const errorText = document.getElementById('error');
+
+// array sorting types
+function bubbleSortfunction(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+function selectionSortFunction(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+
+function insertionSortFunction(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+
+function mergeSortFunction(arr) {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSortFunction(arr.slice(0, mid));
+    const right = mergeSortFunction(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let res = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length &&
+            rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            res.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            res.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return res.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+function calculateWithCacheFunction(sortFunction) {
+    const cache = {};
+
+    return function calculate(input) {
+        const cacheKey = input.join(',');
+
+        const result = sortFunction([...input]);
+        cache[cacheKey] = result; 
+        return result;
+    };
+}
+
+const cachedBubbleSort = calculateWithCacheFunction(bubbleSortfunction);
+const cachedSelectionSort = calculateWithCacheFunction(selectionSortFunction);
+const cachedInsertionSort = calculateWithCacheFunction(insertionSortFunction);
+const cacheMergeSort = calculateWithCacheFunction(mergeSortFunction);
+
+function numbersIsSorted(cachedSort) {
+    const inputValue = input.value.trim();
+    errorText.innerText = "";
+
+    if (!inputValue) {;
+        errorText.innerText = "No numbers to sort.";
+        result.innerText = "";
+        return;
+    }
+
+    const filledInputNums = inputValue.split(","); // const filledInputNumbers = inputValue.split(" ")
+    const inputArray = [];
+    for (let i = 0; i < filledInputNums.length; i++) {
+        inputArray.push(Number(filledInputNums[i]));
+    }
+
+    if (inputArray.some(isNaN)) {
+        errorText.innerText = "Please enter only valid numbers.";
+        result.innerText = "";
+        return;
+    }
+
+    const sortedNumbers = cachedSort(inputArray);
+
+    if (sortedNumbers.length === 0) {
+        errorText.innerText = "No numbers to sort.";
+    } else {
+        result.innerText = "Sorted Numbers: " + sortedNumbers.join(', ');
+    }
+}
+
+bubbleSortBtn.addEventListener("click", () => numbersIsSorted(cachedBubbleSort));
+selectionSortBtn.addEventListener("click", () => numbersIsSorted(cachedSelectionSort));
+insertionSortBtn.addEventListener("click", () => numbersIsSorted(cachedInsertionSort));
+mergeSortBtn.addEventListener("click", () =>  numbersIsSorted(cacheMergeSort));
